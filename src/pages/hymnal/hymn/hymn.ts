fb558db2ca1997c880b4b5a8a305f401c15d94fa
Jermaine McFarlane <j.mcfarlane.mbbs@gmail.com>
2016-10-28 22:44:15 -0500

    feat(Hymn): rename midi files, detect matching midi present
    Midi iOS and Android plugin installed

--- src/pages/hymnal/hymn/hymn.ts
+++ src/pages/hymnal/hymn/hymn.ts
@@ -10,17 +10,56 @@ export class Hymn {
   hymn: any;
   otherVerses: Array<string>;
   favorite: any;
+  midi: any = {
+    root: "assets/media/midi/",
+    title: "",
+    exists: false,
+    path: ""
+  };
 
   constructor(
     public viewCtrl: ViewController,
     navParams: NavParams,
     private faves: HymnalFaves
   ) {
-    this.hymn = navParams.get('hymn');
-    this.otherVerses = this.getOtherVerses(this.hymn.verses);
-    this.hymnal = navParams.get('from');
-    this.faves.checkIsFavorite(this.hymn, this.hymnal)
-               .then(isFavorite => this.favorite = isFavorite);
+      this.hymn = navParams.get('hymn');
+      this.otherVerses = this.getOtherVerses(this.hymn.verses);
+      this.hymnal = navParams.get('from');
+      this.faves.checkIsFavorite(this.hymn, this.hymnal)
+                 .then(isFavorite => this.favorite = isFavorite);
+      this.initMidi();
+      console.log("Title: "+this.midi.title+" exists - "+this.midi.exists);
+  }
+
+  initMidi() {
+    this.midi.title = this.getMidiTitle(this.hymn);
+    this.midi.path = this.midi.root+this.midi.title+".mid";
+    this.midi.exists = this.midiExists(this.midi.path);
+  }
+
+  //Get the title of the midi file
+  getMidiTitle(hymn): string {
+    //Get title in lowercase
+    //Remove punctuation [",", "!", "?"] and Replace special characters ["'", "-", " "] with _
+    return hymn.title.toLowerCase()
+                     .replace(/[,!\?]/g, "")
+                     .replace(/[,-\s]/g,"_");
+  }
+
+  midiExists(path): boolean {
+    if(path){
+        let req = new XMLHttpRequest();
+        req.open('HEAD', path, false);
+        req.send();
+        return req.status==200;
+    } else {
+        return false;
+    }
+  }
+
+  //Play the hymn midi
+  playHymn() {
+    console.log("Playing Midi");
   }
 
   // Gets the other verses besides Verse 1 and the chorus
